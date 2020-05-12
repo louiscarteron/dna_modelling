bbf649a5df983f057d1d078d2ac2b890f51b05c4
ljc216 <louiscarteron@gmail.com>
2020-05-12 17:44:24 +0100

    Start processing of csv

---
+++ modelling_script/input_sequences.py
@@ -0,0 +1,19 @@
+import csv
+
+def process_csv(path):
+
+  data = []
+
+  with open(path) as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    for r in csv_reader:
+      data.append(r[1])
+
+  return data
+
+def main():
+  oligos = process_csv("data/3xr6.csv")
+  print(len(oligos))
+
+if __name__ == "__main__":
+  main()
