38f22ba461a83bc9a1902ac26bf2a47a1f6c3236
ljc216 <louiscarteron@gmail.com>
2020-05-20 20:18:20 +0100

    Implement hungarian algo to get shortest distances

--- modelling_script/process_fastq.py
+++ modelling_script/process_fastq.py
@@ -3,19 +3,32 @@ from statistics import mean
 from Levenshtein import distance, editops
 from csv import reader, DictReader
 from collections import defaultdict, Counter
+from munkres import Munkres, print_matrix
+from itertools import islice
 import json
 
-filename = '/mnt/c/Users/Louis/Documents/DnaModelling/42k_data/output_reads.fastq'
+#filename = '/mnt/c/Users/Louis/Documents/DnaModelling/42k_data/output_reads.fastq'
 #filename = 'data/porechop/eins3_data_chopped.fastq'
+filename = "data/42k2b/42k2b_porechop_nomiddle.fastq"
 
 '''
 Parses the input file 'filename' and filters it based on a length greater than 'length' and a quality score greater than 'score'
 '''
-def trim_inputs(length, score):
+def trim_inputs(minlength, maxlength, score):
   input_seq_iterator = SeqIO.parse(filename, "fastq")
-  short_seq_iterator = (record for record in input_seq_iterator if len(record.seq) > length and mean(record.letter_annotations['phred_quality']) > score)
+  short_seq_iterator = (record for record in input_seq_iterator if len(record.seq) > minlength and len(record.seq) < maxlength and mean(record.letter_annotations['phred_quality']) > score)
 
-  SeqIO.write(short_seq_iterator, "data/porechop/quality_seq_42k_qscore10.fastq", "fastq")
+  SeqIO.write(short_seq_iterator, "data/42k2b/trimmed/42k2b_porechop_nomiddle_q10.fastq", "fastq")
+
+def trim_inputs_mod(length, score):
+  input_seq_iterator = SeqIO.parse(filename, "fastq")
+
+  temp = ("".join([o for i, o in enumerate(str(record.seq)) if record.letter_annotations['phred_quality'][i] > score]) for record in input_seq_iterator )
+  short_temp = (record for record in temp if len(record) > length)
+
+  #short_seq_iterator = (record for record in input_seq_iterator if len(record.seq) > length and mean(record.letter_annotations['phred_quality']) > score)
+  return short_temp
+  #SeqIO.write(short_temp, "data/porechop/test.fastq", "fastq")
 
 
 def _read_csv(filepath):
@@ -48,7 +61,7 @@ def _read_input(filepath):
 def match(input_oligos, read_oligos):
   report = []
 
-  short = input_oligos[:100]
+  short = input_oligos[:1000]
 
   for i in short:
 
@@ -75,20 +88,77 @@ def match(input_oligos, read_oligos):
 
   return report
 
+class GeneratorLen(object):
+  def __init__(self, gen, length):
+    self.gen = gen
+    self.length = length
+
+  def __len__(self): 
+    return self.length
+
+  def __iter__(self):
+    return self.gen
+
+  def __getitem__(self, key):
+    if isinstance(key, int) and key >= 0:
+      return islice(self.gen, key, key + 1)
+    elif isinstance(key, slice):
+      return islice(self.gen, key.start, key.stop, key.step)
+    else:
+      raise KeyError(f'Key must be non-negative integer or slice, not {key}')
+
+def match_try(input_oligos, read_oligos):
+  distance_matrix = []
+
+  print(len(read_oligos))
+
+  short_input = input_oligos[:5]
+  short_read = read_oligos[:20000]
+
+  for i in short_input:
+    distance_matrix.append(GeneratorLen((distance(i, r) for r in short_read), 20000))
+
+  print("done")
+
+  m = Munkres()
+  indexes = m.compute(distance_matrix)
+
+  print("done")
+
+  report = []
+
+  for row, column in indexes:
+    io = short_input[row]
+    ro = short_read[column]
+
+    edits = editops(io, ro)
+    counts = Counter(x[0] for x in edits)
+
+    report.append({
+      'input_oligo': io,
+      'match': {
+        'score': distance(io, ro),
+        'closest_match': ro,
+        'edits': dict(counts)
+      }
+    })
+    
+  return report
+
 
 def process_report(report):
   temp = [r['match']['score'] for r in report]
   print(mean(temp))
 
 def dump_report(report):
-  with open("report_42k_qscore10_1.json", "w+") as fp:
+  with open("data/42k2b/reports/report_notrim_42k2b.json", "w+") as fp:
     json.dump(report, fp, indent = 2)
 
 def main():
-  #trim_inputs(90, 10)
+  #trim_inputs(90, 150, 10)
   #return
   input_oligos = _read_csv("data/3xr6.csv")
-  read_oligos = _read_input("data/porechop/quality_seq_42k_qscore10.fastq")
+  read_oligos = _read_input("data/42k2b/42k2b.fastq")
   report = match(input_oligos, read_oligos)
   process_report(report)
   dump_report(report)
